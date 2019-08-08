5df4673e9bd289f2668a6432f35724c5dc2ae3f9
gergokutu <gergo.kutu@gmail.com>
2019-08-08 17:21:51 +0200

    React setup, koa, cors, grapql, apollo » movies sent to client side

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
+import ApolloClient, { gql } from "apollo-boost"
+import { ApolloProvider, Query } from "react-apollo"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+})
+
+const MOVIES_QUERY = gql`
+  {
+    movies {
+      id
+      title
+      release_date
+    }
+  }
+`
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Query query={MOVIES_QUERY}>
+            {({ loading, error, data }) => {
+              if (loading) return <p>Loading...</p>
+              if (error) return <p>Error :(</p>
+              return this.renderMovies(data.movies)
+            }}
+          </Query>
+        </div>
+      </ApolloProvider>
+    )
+  }
+
+  renderMovies(movies) {
+    return movies.map(movie => <div key={movie.id}>{movie.title}</div>)
+  }
 }
 
-export default App;
+export default App
